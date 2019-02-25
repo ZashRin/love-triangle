8b9aab2ffb093b7d4bae963ce7cb9edc5ff4749c
ZashRin <itgtktdbx@gmail.com>
2019-02-25 16:52:09 +0300

    Solved task for 100%

--- src/index.js
+++ src/index.js
@@ -4,4 +4,26 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let count = 0;
+  let first, second, third;
+  preferences.unshift(NaN);
+
+  for(first = 0; first < preferences.length; first++)
+  {
+    if(preferences[first] === NaN) continue;
+    second = preferences[first];
+    if(preferences[second] === NaN) continue;
+    third = preferences[second];
+    if(preferences[third] === NaN) continue;
+    if(first == second || second == third) continue;
+    
+    if(preferences[third] == first) {
+      count++;
+      preferences[first] = NaN;
+      preferences[second] = NaN;
+      preferences[third] = NaN;
+    }
+  }
+
+  return count;
 };
